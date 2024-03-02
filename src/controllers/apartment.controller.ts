ba802ec75b6eaeef12cb57b0ebf6d37579faf282
mazenaboelanin <mazenaboelanin@gmail.com>
2024-03-02 15:52:55 +0200

    added title of the apartment to create apartment

--- src/controllers/apartment.controller.ts
+++ src/controllers/apartment.controller.ts
@@ -33,9 +33,10 @@ export const getApartment: RequestHandler = async(req, res: { json: (arg0: { suc
 // @route      GET api/v1/apartments
 // @access     Public
 export const createApartment: RequestHandler = async(req, res: { json: (arg0: { success: boolean; msg: string; response?: any; err?: any; }) => void; }, next)=>{
-  const {imageUrl, description, area, compound, price, city, phoneNumber, paymentType, finished} = req?.body;
+  const {title, imageUrl, description, area, compound, price, city, phoneNumber, paymentType, finished} = req?.body;
   try {
     const newApartment = await Apartment.create({
+      title,
       imageUrl,
       description,
       area,
