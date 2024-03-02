3c6e1a04bde9b7149aaa0a16de7aa4c308607391
mazenaboelanin <mazenaboelanin@gmail.com>
2024-03-02 15:36:23 +0200

    added create apartment controller

--- src/controllers/apartment.controller.ts
+++ src/controllers/apartment.controller.ts
@@ -29,3 +29,27 @@ export const getApartment: RequestHandler = async(req, res: { json: (arg0: { suc
 }
 
 
+// @desc       create new Apartment
+// @route      GET api/v1/apartments
+// @access     Public
+export const createApartment: RequestHandler = async(req, res: { json: (arg0: { success: boolean; msg: string; response?: any; err?: any; }) => void; }, next)=>{
+  const {imageUrl, description, area, compound, price, city, phoneNumber, paymentType, finished} = req?.body;
+  try {
+    const newApartment = await Apartment.create({
+      imageUrl,
+      description,
+      area,
+      compound,
+      price,
+      city,
+      phoneNumber,
+      paymentType,
+      finished
+    });
+
+    res.json({success: true, msg: "Create Apartment", response: newApartment});
+  } catch (err) {
+      res.json({success: false, msg: "Error", err});
+  }
+}
+
