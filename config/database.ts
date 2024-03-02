087e35c28d6c9425858b10be4389630e39421cfc
mazenaboelanin <mazenaboelanin@gmail.com>
2024-03-02 16:02:50 +0200

    get db credential from env variables

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize';
 
-const database = new Sequelize('realestate', 'postgres','1234', {
-    host : 'localhost',
+const database = new Sequelize( process.env.DB_NAME,  process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host : process.env.DB_HOST,
     dialect: 'postgres',
     pool : {
         max : 5,
