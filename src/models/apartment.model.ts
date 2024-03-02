dde416712f70049531060277b925a9aa6efe72c6
mazenaboelanin <mazenaboelanin@gmail.com>
2024-03-02 15:42:42 +0200

    re export apartment model

--- src/models/apartment.model.ts
+++ src/models/apartment.model.ts
@@ -1,7 +1,9 @@
 import ApartmentSchema  from '../schemas/apartment.schema';
 
-export const ApartmentModel = ApartmentSchema;
+const ApartmentModel = ApartmentSchema;
 
 ApartmentModel.sync({ force: true })
   .then(() => console.log('Apartment table created'))
   .catch(error => console.error('Failed to create Apartment table:', error));
+
+  export default ApartmentModel;
