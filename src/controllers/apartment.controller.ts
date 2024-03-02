400e67c32cfe73c20f565b30863251bcf101f2cb
mazenaboelanin <mazenaboelanin@gmail.com>
2024-03-02 15:36:02 +0200

    added get specific apartment controller

--- src/controllers/apartment.controller.ts
+++ src/controllers/apartment.controller.ts
@@ -14,3 +14,18 @@ export const getAllApartments: RequestHandler = async(req, res: { json: (arg0: {
 }
 
 
+// @desc       get specific Apartment
+// @route      GET api/v1/apartments/:id
+// @access     Public
+export const getApartment: RequestHandler = async(req, res: { json: (arg0: { success: boolean; msg: string; response?: any; err?: any; }) => void; }, next)=>{
+  const {id} = req.params;
+  try {
+    const apartment = await Apartment.findByPk(id);
+
+    res.json({success: true, msg: "get Apartment", response: apartment});
+  } catch (err) {
+      res.json({success: false, msg: "Error", err});
+  }
+}
+
+
