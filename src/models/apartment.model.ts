5679d504ac00c02c42ac54c9835681c64cbcd11e
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-06-11 02:08:58 +0300

    add seeds

--- src/models/apartment.model.ts
+++ src/models/apartment.model.ts
@@ -1,9 +1,13 @@
 import ApartmentSchema  from '../schemas/apartment.schema';
+import { apartmentsSeed } from './apartmentsSeed';
 
 const ApartmentModel = ApartmentSchema;
 
 ApartmentModel.sync({ force: true })
-  .then(() => console.log('Apartment table created'))
+  .then(async () => { 
+    console.log('Apartment table created');
+    await ApartmentModel.bulkCreate(apartmentsSeed);
+  })
   .catch(error => console.error('Failed to create Apartment table:', error));
 
   export default ApartmentModel;
---
+++ src/models/apartmentsSeed.ts
@@ -0,0 +1,115 @@
+export const apartmentsSeed =
+
+[
+  {
+    title: "Chalet - Hacienda West",
+    imageUrl: "https://img-v2.gtsstatic.net/reno/imagereader.aspx?imageurl=https%3A%2F%2Fsir.azureedge.net%2F1103i215%2Fzq1by69g7dfk4djfqjt68q3be5i215&option=N&h=472&permitphotoenlargement=false",
+    description: {
+      info: "A 3 bedroom Chalet in Hacienda West by Palm Hills Developments. The Chalet size is 215 m2 with 4 bathrooms and a roof of 84 m2",
+      rooms: 3,
+      bathrooms: 2,
+      kitchens: 1,
+      reception: 3
+    },
+    area: "150",
+    compound: "Hacienda West",
+    price: 1900000,
+    phoneNumber: "01223841629",
+    city: "October City",
+    paymentType: "Installment",
+    finished: false,
+    finishedDate: new Date("10-6-2025")
+  },
+  {
+    title: "Villa - Hacienda West",
+    imageUrl: "https://i0.wp.com/esalesinternational.com/wp-content/uploads/2022/05/sell-villa-in-Egypt.jpg?fit=1024%2C768&ssl=1",
+    description: {
+      info: "A 3 bedroom Chalet in Hacienda West by Palm Hills Developments. The Chalet size is 215 m2 with 4 bathrooms and a roof of 84 m2",
+      rooms: 3,
+      bathrooms: 2,
+      kitchens: 1,
+      reception: 3
+    },
+    area: "160",
+    compound: "Hacienda West",
+    price: 2000000,
+    phoneNumber: "01223841629",
+    city: "October City",
+    paymentType: "cash",
+    finished: false,
+    finishedDate: new Date("10-6-2025")
+  },
+  {
+    title: "Villa - October City",
+    imageUrl: "https://www.propertyfinder.eg/property/68601bfc7e8ce10ceb197cc131deceb7/668/452/MODE/631975/5356380-4e4a7o.jpg?ctr=eg",
+    description: {
+      info: "A 3 bedroom Chalet in Hacienda West by Palm Hills Developments. The Chalet size is 215 m2 with 4 bathrooms and a roof of 84 m2",
+      rooms: 3,
+      bathrooms: 2,
+      kitchens: 1,
+      reception: 3
+    },
+    area: "180",
+    compound: "6 October",
+    price: 2500000,
+    phoneNumber: "01223841629",
+    city: "October City",
+    paymentType: "Installment",
+    finished: false,
+    finishedDate: new Date("10-6-2025")
+  },
+  {
+    title: "Villa - New Cairo",
+    imageUrl: "https://img-v2.gtsstatic.net/reno/imagereader.aspx?imageurl=https%3A%2F%2Fsir.azureedge.net%2F1103i215%2Fskjx7xzeyrfc42afj6r6dmg5k4i215&option=N&h=472&permitphotoenlargement=false",
+    description: {
+      info: "A 5 bedroom Chalet in Hacienda West by Palm Hills Developments. The Chalet size is 215 m2 with 4 bathrooms and a roof of 84 m2",
+      rooms: 5,
+      bathrooms: 4,
+      kitchens: 2,
+      reception: 3
+    },
+    area: "170",
+    compound: "New Cairo",
+    price: 2200000,
+    phoneNumber: "01223841629",
+    city: "October City",
+    paymentType: "cash",
+    finished: true,
+  },
+  {
+    title: "Villa - New Cairo",
+    imageUrl: "https://img-v2.gtsstatic.net/reno/imagereader.aspx?imageurl=https%3A%2F%2Fsir.azureedge.net%2F1103i215%2Fskjx7xzeyrfc42afj6r6dmg5k4i215&option=N&h=472&permitphotoenlargement=false",
+    description: {
+      info: "A 5 bedroom Chalet in Hacienda West by Palm Hills Developments. The Chalet size is 215 m2 with 4 bathrooms and a roof of 84 m2",
+      rooms: 5,
+      bathrooms: 4,
+      kitchens: 2,
+      reception: 3
+    },
+    area: "140",
+    compound: "New Cairo",
+    price: 1500000,
+    phoneNumber: "01223841629",
+    city: "October City",
+    paymentType: "cash",
+    finished: true
+  },
+  {
+    title: "Villa - New Cairo",
+    imageUrl: "https://img-v2.gtsstatic.net/reno/imagereader.aspx?imageurl=https%3A%2F%2Fsir.azureedge.net%2F1103i215%2Fskjx7xzeyrfc42afj6r6dmg5k4i215&option=N&h=472&permitphotoenlargement=false",
+    description: {
+      info: "A 5 bedroom Chalet in Hacienda West by Palm Hills Developments. The Chalet size is 215 m2 with 4 bathrooms and a roof of 84 m2",
+      rooms: 5,
+      bathrooms: 4,
+      kitchens: 2,
+      reception: 3
+    },
+    area: "130",
+    compound: "New Cairo",
+    price: 1200000,
+    phoneNumber: "01223841629",
+    city: "October City",
+    paymentType: "cash",
+    finished: true,
+  }
+]
